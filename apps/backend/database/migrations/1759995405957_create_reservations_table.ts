d21b4932690f7511683924c545adc70e6155457e
Vincent Guyot <guyot.vincent18@gmail.com>
2025-10-09 12:15:40 +0200

    migration table user, reservations, equipment

---
+++ apps/backend/database/migrations/1759995405957_create_reservations_table.ts
@@ -0,0 +1,21 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'reservations'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.uuid('id').notNullable().primary()
+      table.datetime('start_date').notNullable()
+      table.datetime('end_date').notNullable()
+      table.enum('status', ['waiting', 'confirmed', 'cancelled']).notNullable().defaultTo('waiting')
+      table.string('sport_equipment_id').notNullable()
+      table.uuid('user_id').references('id').inTable('users').onDelete('CASCADE')
+      table.timestamp('created_at').notNullable()
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
