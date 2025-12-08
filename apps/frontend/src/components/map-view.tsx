7e05bc63e2b349b7345c5af4e849cf543ee3f0d5
Vincent Guyot <guyot.vincent18@gmail.com>
2025-12-08 11:29:52 +0100

    map bien

--- apps/frontend/src/components/map-view.tsx
+++ apps/frontend/src/components/map-view.tsx
@@ -3,25 +3,34 @@ import { SportPlaceItem } from "./sport-place-item"
 import { SportEquipment } from "~/lib/queries/sport-equipments";
 import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 export function MapView({ results }: { results: SportEquipment[] }) {
-    return (
-        <div className="h-screen w-1/2 rounded-b-xl overflow-hidden">
-          <MapContainer
-            center={[48.8566, 2.3522]}
-            zoom={13}
-            scrollWheelZoom={true}
-            style={{ height: "100%", width: "100%" }}
-          >
-            <TileLayer
-              attribution='&copy; OpenStreetMap contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
+  
+  return (
+    <div className="h-screen w-3/4 rounded-b-xl overflow-hidden mb-5 mt-5">
+      <MapContainer
+        center={[48.8566, 2.3522]}
+        zoom={6}
+        scrollWheelZoom={false}
+        className="h-full w-full"
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>'
+          url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
+        />
 
-            <Marker position={[48.8566, 2.3522]}>
-              <Popup>Paris ✨</Popup>
-            </Marker>
-          </MapContainer>
-        </div>
-    )
+        <Marker position={[48.8566, 2.3522]}>
+          <Popup>
+            <div className="p-3 rounded-xl shadow-lg bg-white text-gray-800 max-w-[200px] min-w-[150px]">
+              <h3 className="font-semibold text-lg mb-1">Paris ✨</h3>
+              <p className="text-sm text-gray-600">
+                Capitale de la France, ville lumière.
+              </p>
+            </div>
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  )
 }
